5ad6645de047f84a2d3e0e5100f3b0c69866116a
Alexey <oleksii.ovchynnikov@nure.ua>
2017-06-25 16:36:26 +0300

    commit in newBranch. before merging withmaster

--- BlackJack/GameEngine.cs
+++ BlackJack/GameEngine.cs
@@ -11,6 +11,7 @@ namespace BlackJack
         public static int MAX_NUM_CARDS = 8; //maximum number of cards
         public static int ENEMY_STOP_SCORE = 17; //enemy stops score
         public static int WIN_SCORE = 21;//main score of game
+        public static string str = "11";
 
         internal Stack<int> GetSuit() //Deck generation
         {
